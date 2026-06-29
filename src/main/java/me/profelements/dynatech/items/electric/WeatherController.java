77df06a979ca0dcea617f7ebf8ecc4fbaa2f9103
lipcyy <ripzzbg@gmail.com>
2026-06-29 18:18:22 +0700

    Update WeatherController.java

--- src/main/java/me/profelements/dynatech/items/electric/WeatherController.java
+++ src/main/java/me/profelements/dynatech/items/electric/WeatherController.java
@@ -125,13 +125,13 @@ public class WeatherController extends AbstractElectricTicker implements RecipeD
     public List<ItemStack> getDisplayRecipes() {
         List<ItemStack> items = new ArrayList<>();
         items.add(new ItemStack(Material.SUNFLOWER));
-        items.add(new CustomItemStack(Material.DIAMOND, "&f让费城充满阳光。"));
+        items.add(new CustomItemStack(Material.DIAMOND, "&fLet Philadelphia be filled with sunshine."));
 
         items.add(new ItemStack(Material.LILAC));
-        items.add(new CustomItemStack(Material.DIAMOND, "&f老人打鼾时下雨。"));
+        items.add(new CustomItemStack(Material.DIAMOND, "&fRain while the old man snores."));
 
         items.add(new ItemStack(Material.CREEPER_HEAD));
-        items.add(new CustomItemStack(Material.DIAMOND, "&f雷电！"));
+        items.add(new CustomItemStack(Material.DIAMOND, "&fThunder!"));
 
         return items;
     }
