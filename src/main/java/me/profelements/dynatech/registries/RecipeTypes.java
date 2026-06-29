dbd6b60e507d4bea5f5ffdc856017261e5c2747c
lipcyy <ripzzbg@gmail.com>
2026-06-29 18:30:43 +0700

    Update RecipeTypes.java

--- src/main/java/me/profelements/dynatech/registries/RecipeTypes.java
+++ src/main/java/me/profelements/dynatech/registries/RecipeTypes.java
@@ -24,16 +24,16 @@ public class RecipeTypes {
     }
 
     public static final RecipeType SCOOPING = new RecipeType(Keys.SCOOPING.key(),
-            new CustomItemStack(Material.IRON_SHOVEL, "使用勺子获取该物品。"));
+            new CustomItemStack(Material.IRON_SHOVEL, "Use a Scoop to obtain this item."));
 
     public static final RecipeType OVENING = new RecipeType(Keys.OVENING.key(),
-            new CustomItemStack(Material.SMOKER, "丢入焦炭炉"));
+            new CustomItemStack(Material.SMOKER, "Throw into the Coal Coke Oven"));
 
     public static final RecipeType BLOCK_DROP = new RecipeType(Keys.BLOCK_DROP.key(),
-            new CustomItemStack(Material.COBWEB, "从方块掉落"));
+            new CustomItemStack(Material.COBWEB, "Dropped from blocks"));
 
     public static final RecipeType TREE_GROWTH_CHAMBER = new RecipeType(Keys.TREE_GROWTH_CHAMBER.key(),
-            new CustomItemStack(Material.LIME_CONCRETE, "放入树木生长加速器"));
+            new CustomItemStack(Material.LIME_CONCRETE, "Place in Tree Growth Chamber"));
 
     public static final RecipeType MATERIAL_HIVE = new RecipeType(Keys.MATERIAL_HIVE.key(),
             Items.MATERIAL_HIVE.stack(),
